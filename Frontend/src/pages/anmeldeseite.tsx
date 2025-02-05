511bd2224c272933c831615219c51c9ec37f26b8
matthias <matthias_hauswirth@hotmail.de>
2025-02-05 18:42:57 +0100

    verbesserungen

--- Frontend/src/pages/anmeldeseite.tsx
+++ Frontend/src/pages/anmeldeseite.tsx
@@ -1,45 +1,64 @@
 import React, { useState } from 'react';
+import { TextField, Stack, Button, FormControl, InputLabel, OutlinedInput, IconButton,InputAdornment } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 export function Anmeldeseite() {
   
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState(false);
 
-  const anmeldung = (event: React.FormEvent) => {
-    event.preventDefault();
+  const anmeldung = () => {
+    setError(true);
     // Handle login logic here
     console.log('Email:', email);
     console.log('Password:', password);
   };
   
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  
+
+  //Header fehlt noch, styling fehlt noch, auch überlegen ob es nicht was besseres als h2 gibt... 
   return(
     <div style={{ maxWidth: '400px', margin: 'auto'}}>
       <h2>Login</h2>
-      <form onSubmit={anmeldung}>
-        <div style={{ marginBottom: '10px' }}>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
+        <Stack spacing={2} direction="column">
+          <TextField
+            error={email === "" && error}
+            required 
             id="email"
+            label="Email:"
+            placeholder="example@example.com"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            style={{ width: '100%', padding: '10px', marginTop: '5px' }}
-          />
-        </div>
-        <div style={{ marginBottom: '10px' }}>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
+            onChange={(event) => setEmail(event.target.value)}/>
+        <FormControl variant="outlined">
+          <InputLabel htmlFor="password" error={password === "" && error}>Password</InputLabel>
+          <OutlinedInput
             id="password"
+            error={password === "" && error}
+            required 
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label={
+                    showPassword ? 'hide the password' : 'display the password'
+                  }
+                  onClick={handleClickShowPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            style={{ width: '100%', padding: '10px', marginTop: '5px' }}
+            onChange={(event) => setPassword(event.target.value)}
           />
-        </div>
-        <button type="submit" style={{ padding: '5px' }}>Login</button>
-      </form>
+        </FormControl>
+        <Button onClick={anmeldung}>Login</Button>
+        </Stack>
     </div>
   );
 };
