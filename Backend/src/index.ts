7e347ee6df8362286f2e444727aa11bd543dbb56
matthias <matthias_hauswirth@hotmail.de>
2025-05-19 13:59:09 +0200

    Beispiel

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 import {db} from "./db"
 import {set} from "./db/schema";
 
 const app = express();
+
+app.use(cors());
 
 app.get("/", (_, res) => { 
   res.send("Hello express");
@@ -11,5 +14,14 @@ app.get("/", (_, res) => {
 db.select().from(set).then((data) => {
   console.log("Data from database:", data);
 });
+
+app.post("/getQuestion", (_, res) => {
+  res.json(
+    {
+      id: 1,
+      question: "Was ist der Unterschied zwischen einer Katze und einem Hund?",
+      answer: "Hund heißt Matthias"
+    });
+  });
 app.listen(80);
 console.log("Server started at http://localhost:80");
