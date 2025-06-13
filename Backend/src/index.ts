e4ef0cdcdb7cd040c9d6b6def50924fbc4f8688d
David <david.frommann@swr.de>
2025-06-13 12:11:25 +0200

    fix for linter

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -22,5 +22,14 @@ app.get("/", (_, res) => {
   res.send("Hello express");
 });
 
+
+app.post("/getQuestion", (_, res) => {
+  res.json(
+    {
+      id: 1,
+      question: "Was ist der Unterschied zwischen einer Katze und einem Hund?",
+      answer: "Hund heißt Matthias"
+    });
+  });
 app.listen(80);
 console.log("Server started at http://localhost:80");
