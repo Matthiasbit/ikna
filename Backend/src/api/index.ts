59c89071845097c3798abaadfc304b098fd8070c
matthias <matthias_hauswirth@hotmail.de>
2025-06-25 18:01:11 +0200

    vercel try number 10

--- Backend/src/api/index.ts
+++ Backend/src/api/index.ts
@@ -20,4 +20,7 @@ app.get("/", (_, res) => {
   res.send("Hello express");
 });
 
-module.exports.handler = serverless(app);
+
+app.listen(3000, () => console.log('Server ready on port 3000.'));
+
+module.exports = app;
