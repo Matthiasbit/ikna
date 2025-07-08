28d0bea2d6fc30b46912768d88158f940391050b
matthias <matthias_hauswirth@hotmail.de>
2025-07-08 16:55:15 +0200

    enter als eingabe

--- Frontend/src/pages/loginpage.tsx
+++ Frontend/src/pages/loginpage.tsx
@@ -9,14 +9,13 @@ import { useRouter } from 'next/navigation';
 
 
 export function Loginpage() {
-  
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
-const [showPassword, setShowPassword] = useState(false);
-const [validationError, setValidationError] = useState(false);
-const [isResetPassword, setIsResetPassword] = useState(false);
-const [tabValue, setTabValue] = useState<"1" | "2">("1");
-const { registration, errormessage, setErrorMessage} = useRegistration();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [validationError, setValidationError] = useState(false);
+  const [isResetPassword, setIsResetPassword] = useState(false);
+  const [tabValue, setTabValue] = useState<"1" | "2">("1");
+  const { registration, errormessage, setErrorMessage} = useRegistration();
   const router = useRouter();
 
   async function logInOrSignUp(signUp: boolean) {
@@ -28,96 +27,48 @@ const { registration, errormessage, setErrorMessage} = useRegistration();
   }
   
   const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-  const handleResetLinkClick = () => {
-    setIsResetPassword(!isResetPassword);
-  };
+    const handleResetLinkClick = () => {
+      setIsResetPassword(!isResetPassword);
+    };
 
   return (
-    <div>
-      <Header text={tabValue === "1" ? isResetPassword ? "Reset Password" : "Login" : "Sign up" } />
-      <Box sx={{ width: '100%',  marginTop: '15vh' }}>
-        <Box sx={{ maxWidth: { xs: '90%', sm: '70%', md: '50%', lg: '30%' }, margin: 'auto'}}>
-          <TabContext value={tabValue}>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-              <TabList onChange={(_event, value) => {setTabValue(value); setErrorMessage(null);}} centered>
-                <Tab label="Login" value="1"/>
-                <Tab label="Sign Up" value="2" />
-              </TabList>
-            </Box>
-            <TabPanel value="1">
-              <Typography variant="body1" gutterBottom>
-                { isResetPassword ? (
-                  <>
-                    Enter your email to reset your password. Or Log in <Link href="#" onClick={handleResetLinkClick}>here</Link>.
-                  </>
-                ) : (
-                  <>
-                    Log in to an existing account. Or reset your Password <Link href="#" onClick={handleResetLinkClick}>here</Link>.
-                  </>
-                )}
-              </Typography>
-              <Stack spacing={2} direction="column">
-                <TextField
-                  error={email === "" && validationError}
-                  id="email"
-                  label="Email:"
-                  placeholder="example@example.com"
-                  value={email}
-                  onChange={(event) => setEmail(event.target.value)} />
-                {!isResetPassword && (
-                  <FormControl variant="outlined">
-                    <InputLabel htmlFor="password" error={password === "" && validationError}>Password</InputLabel>
-                    <OutlinedInput
-                      id="password"
-                      error={password === "" && validationError}
-                      type={showPassword ? 'text' : 'password'}
-                      endAdornment={
-                        <InputAdornment position="end">
-                          <IconButton
-                            aria-label={showPassword ? 'hide the password' : 'display the password'}
-                            onClick={handleClickShowPassword}
-                            edge="end"
-                          >
-                            {showPassword ? <VisibilityOff /> : <Visibility />}
-                          </IconButton>
-                        </InputAdornment>
-                      }
-                      label="Password"
-                      value={password}
-                      onChange={(event) => setPassword(event.target.value)}
-                    />
-                  </FormControl>
-                )}
-                <Button onClick={() => logInOrSignUp(false)} variant="contained" color="primary" type="button">
-                  {isResetPassword ? "Reset Password" : "Login"}
-                </Button>
-                {errormessage && (
-                <Typography color="error" sx={{ mt: 2 }}>
-                  {errormessage}
-                </Typography>
+  <div>
+    <Header text={tabValue === "1" ? isResetPassword ? "Reset Password" : "Login" : "Sign up" } />
+    <Box sx={{ width: '100%',  marginTop: '15vh' }}>
+      <Box sx={{ maxWidth: { xs: '90%', sm: '70%', md: '50%', lg: '30%' }, margin: 'auto'}}>
+        <TabContext value={tabValue}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <TabList onChange={(_event, value) => {setTabValue(value); setErrorMessage(null);}} centered>
+              <Tab label="Login" value="1"/>
+              <Tab label="Sign Up" value="2" />
+            </TabList>
+          </Box>
+          <TabPanel value="1">
+            <Typography variant="body1" gutterBottom>
+              { isResetPassword ? (
+                <>
+                  Enter your email to reset your password. Or Log in <Link href="#" onClick={handleResetLinkClick}>here</Link>.
+                </>
+              ) : (
+                <>
+                  Log in to an existing account. Or reset your Password <Link href="#" onClick={handleResetLinkClick}>here</Link>.
+                </>
               )}
-              </Stack>
-            </TabPanel>
-            <TabPanel value="2">
-              <Typography variant="body1" gutterBottom>
-                Create a new account.
-              </Typography>
-              <Stack spacing={2} direction="column">
-                <TextField
-                  error={email === "" && validationError}
-                  required
-                  id="email"
-                  label="Email:"
-                  placeholder="example@example.com"
-                  value={email}
-                  onChange={(event) => setEmail(event.target.value)} />
+            </Typography>
+            <Stack spacing={2} direction="column">
+              <TextField
+                error={email === "" && validationError}
+                id="email"
+                label="Email:"
+                placeholder="example@example.com"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)} />
+              {!isResetPassword && (
                 <FormControl variant="outlined">
                   <InputLabel htmlFor="password" error={password === "" && validationError}>Password</InputLabel>
                   <OutlinedInput
                     id="password"
                     error={password === "" && validationError}
-                    required
                     type={showPassword ? 'text' : 'password'}
                     endAdornment={
                       <InputAdornment position="end">
@@ -133,22 +84,79 @@ const { registration, errormessage, setErrorMessage} = useRegistration();
                     label="Password"
                     value={password}
                     onChange={(event) => setPassword(event.target.value)}
+                    onKeyDown={(event) => {
+                      if (event.key === 'Enter') {
+                        logInOrSignUp(false);
+                      }
+                    }}
                   />
                 </FormControl>
-                <Button onClick={() => logInOrSignUp(true)} variant="contained" color="primary" type="button">
-                  Sign Up
-                </Button>
-                {errormessage && (
-                  <Typography color="error" sx={{ mt: 2 }}>
-                    {errormessage}
-                  </Typography>
-                )}
-              </Stack>
-            </TabPanel>
-          </TabContext>
-        </Box>
+              )}
+              <Button onClick={() => logInOrSignUp(false)} variant="contained" color="primary" type="button">
+                {isResetPassword ? "Reset Password" : "Login"}
+              </Button>
+              {errormessage && (
+              <Typography color="error" sx={{ mt: 2 }}>
+                {errormessage}
+              </Typography>
+            )}
+            </Stack>
+          </TabPanel>
+          <TabPanel value="2">
+            <Typography variant="body1" gutterBottom>
+              Create a new account.
+            </Typography>
+            <Stack spacing={2} direction="column">
+              <TextField
+                error={email === "" && validationError}
+                required
+                id="email"
+                label="Email:"
+                placeholder="example@example.com"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)} />
+              <FormControl variant="outlined">
+                <InputLabel htmlFor="password" error={password === "" && validationError}>Password</InputLabel>
+                <OutlinedInput
+                  id="password"
+                  error={password === "" && validationError}
+                  required
+                  type={showPassword ? 'text' : 'password'}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label={showPassword ? 'hide the password' : 'display the password'}
+                        onClick={handleClickShowPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                  onKeyDown={(event) => {
+                      if (event.key === 'Enter') {
+                        logInOrSignUp(true);
+                      }
+                    }}
+                />
+              </FormControl>
+              <Button onClick={() => logInOrSignUp(true)} variant="contained" color="primary" type="button">
+                Sign Up
+              </Button>
+              {errormessage && (
+                <Typography color="error" sx={{ mt: 2 }}>
+                  {errormessage}
+                </Typography>
+              )}
+            </Stack>
+          </TabPanel>
+        </TabContext>
       </Box>
-    </div>
+    </Box>
+  </div>
   );
 };
 
